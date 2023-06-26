ae6a0878ffbc76552032562270cd6b9e92c1591e
DennisEduard01 <dennis.eduard01@yahoo.com>
2023-06-26 22:01:11 +0200

    Create CategoriesService with findAll() method returning categories data. Returns all categories

--- src/categories/categories.service.ts
+++ src/categories/categories.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { Category } from './models/category.model';
+import {categories} from '../../data/categories'
+
 
 @Injectable()
-export class CategoriesService {}
+export class CategoriesService {
+    async findAll(): Promise<Category[]> {
+
+        return categories
+    }
+}
