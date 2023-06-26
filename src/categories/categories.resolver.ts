30da2452080fe2080db5efb87853a263a66d8053
DennisEduard01 <dennis.eduard01@yahoo.com>
2023-06-26 21:58:36 +0200

    Implement CategoriesResolver with getCategories() query method. Returns all the categories

--- src/categories/categories.resolver.ts
+++ src/categories/categories.resolver.ts
@@ -1,4 +1,14 @@
+import {Query} from '@nestjs/graphql';
 import { Resolver } from '@nestjs/graphql';
+import { CategoriesService } from './categories.service';
+import { Category } from './models/category.model';
 
-@Resolver()
-export class CategoriesResolver {}
+@Resolver(of => Category)
+export class CategoriesResolver {
+    constructor(private readonly categoriesService: CategoriesService) {}
+
+    @Query(returns => [Category])
+    getCategories(): Promise<Category[]> {
+        return this.categoriesService.findAll();
+    }
+}
