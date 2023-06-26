3ea31a33b247941d5eec039911ae3520ae0942b8
DennisEduard01 <dennis.eduard01@yahoo.com>
2023-06-27 00:18:14 +0200

    Create query for retrieving a category based on its ID

--- src/categories/categories.resolver.ts
+++ src/categories/categories.resolver.ts
@@ -1,16 +1,31 @@
-import {Query} from '@nestjs/graphql';
-import { Resolver } from '@nestjs/graphql';
+import { Query } from '@nestjs/graphql';
+import { NotFoundException } from '@nestjs/common';
+import { Args, Resolver } from '@nestjs/graphql';
 import { CategoriesService } from './categories.service';
+import { CategoriesArgs } from './dto/categories.args';
 import { Category } from './models/category.model';
 
-@Resolver(of => Category)
+@Resolver((of) => Category)
 export class CategoriesResolver {
-    constructor(private readonly categoriesService: CategoriesService) {}
+  constructor(private readonly categoriesService: CategoriesService) {}
 
-    // Retrieves all categories
-    // Second argument sets the query type name to 'categories'
-    @Query(returns => [Category], {name: 'categories'})
-    getCategories(): Promise<Category[]> {
-        return this.categoriesService.findAll();
+  // Retrieves all categories
+  // Second argument sets the query type name to 'categories'
+  @Query((returns) => [Category], { name: 'categories' })
+  getCategories(): Promise<Category[]> {
+    console.log('here');
+    return this.categoriesService.findAll();
+  }
+
+  @Query((returns) => Category, { name: 'category' })
+  async getCategory(@Args() args: CategoriesArgs): Promise<Category> {
+    const { id } = args;
+    const category = await this.categoriesService.findOneById(id);
+
+    if (!category) {
+      throw new NotFoundException(id);
     }
+
+    return category;
+  }
 }
