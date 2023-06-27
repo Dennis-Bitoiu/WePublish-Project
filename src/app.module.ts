6bcc460e9967f22d81730a293c6c4f75d06fe31d
DennisEduard01 <dennis.eduard01@yahoo.com>
2023-06-27 15:04:31 +0200

    Add publications module to the app imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,12 +5,13 @@ import {GraphQLModule} from '@nestjs/graphql';
 import {ApolloDriver, ApolloDriverConfig} from '@nestjs/apollo';
 import { join } from 'path';
 import {CategoriesModule} from './categories/categories.module';
+import { PublicationsModule } from './publications/publications.module';
 
 @Module({
   imports: [ GraphQLModule.forRoot<ApolloDriverConfig>({
     driver: ApolloDriver,
     autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
-  }), CategoriesModule],
+  }), CategoriesModule, PublicationsModule],
   controllers: [AppController],
   providers: [AppService],
 })
