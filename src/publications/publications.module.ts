afad289198d68d609cffb8fb5199cdceea3a2e27
DennisEduard01 <dennis.eduard01@yahoo.com>
2023-06-27 19:01:01 +0200

    Add the CategoriesModule to the imports list of the Publication module

--- src/publications/publications.module.ts
+++ src/publications/publications.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { PublicationsService } from './publications.service';
 import { PublicationsResolver } from './publications.resolver';
+import { CategoriesModule } from 'src/categories/categories.module';
 
 @Module({
-  providers: [PublicationsService, PublicationsResolver]
+  providers: [PublicationsService, PublicationsResolver],
+  imports: [CategoriesModule],
 })
 export class PublicationsModule {}
