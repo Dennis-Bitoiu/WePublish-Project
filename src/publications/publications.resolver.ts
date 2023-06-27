55c0b73cd1d077f6e10b2296632b4ed4a679d29d
DennisEduard01 <dennis.eduard01@yahoo.com>
2023-06-27 15:52:18 +0200

    Implement resolver to find a publication based on its id

--- src/publications/publications.resolver.ts
+++ src/publications/publications.resolver.ts
@@ -1,14 +1,31 @@
-import { Resolver } from '@nestjs/graphql';
+import { Args, Resolver } from '@nestjs/graphql';
+import { NotFoundException } from '@nestjs/common';
 import { Mutation, Query } from '@nestjs/graphql';
 import { PublicationsService } from './publications.service';
 import { Publication } from './models/publication.model';
+import { PublicationsArgs } from './dto/publications.args';
 
 @Resolver((of) => Publication)
 export class PublicationsResolver {
   constructor(private readonly publicationsService: PublicationsService) {}
 
   @Query((returns) => [Publication], { name: 'publications' })
-  getPublications(): Promise<Publication[]> {
+  async getPublications(): Promise<Publication[]> {
     return this.publicationsService.findAll();
+  }
+
+  @Query((returns) => Publication, { name: 'publication' })
+  async getPublicationById(
+    @Args() args: PublicationsArgs,
+  ): Promise<Publication> {
+    const { id } = args;
+
+    const publication = await this.publicationsService.findOneById(id);
+
+    if (!publication) {
+      throw new NotFoundException(id);
+    }
+
+    return publication;
   }
 }
