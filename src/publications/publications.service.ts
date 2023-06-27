2e24839fb0937c4fc09e1d370f67c5a7bfdd63fe
DennisEduard01 <dennis.eduard01@yahoo.com>
2023-06-27 19:39:23 +0200

    Create service which adds the newly created publication to the publications array

--- src/publications/publications.service.ts
+++ src/publications/publications.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { Publication } from './models/publication.model';
 import { publications } from '../../data/publications';
+import { NewPublicationInput } from './dto/publication.input';
 
 @Injectable()
 export class PublicationsService {
@@ -13,6 +14,15 @@ export class PublicationsService {
       (publication) => publication.id === id,
     );
 
+    return publication;
+  }
+
+  async create(publicationInput: NewPublicationInput) {
+    const publication: Publication = {
+      ...publicationInput,
+    };
+
+    publications.push(publication);
     return publication;
   }
 }
