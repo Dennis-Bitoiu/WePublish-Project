103b193076beded93d05f185baf5fb488d9026b4
DennisEduard01 <dennis.eduard01@yahoo.com>
2023-06-27 13:50:29 +0200

    Implement create service which creates a new category from the received input and adds it to the categories array

--- src/categories/categories.service.ts
+++ src/categories/categories.service.ts
@@ -1,16 +1,25 @@
 import { Injectable } from '@nestjs/common';
 import { Category } from './models/category.model';
-import {categories} from '../../data/categories'
-
+import { categories } from '../../data/categories';
+import { NewCategoryInput } from './dto/category.input';
 
 @Injectable()
 export class CategoriesService {
-    async findAll(): Promise<Category[]> {
+  async findAll(): Promise<Category[]> {
+    return categories;
+  }
 
-        return categories
-    }
+  async findOneById(id: string): Promise<Category> {
+    return categories.find((category) => category.id === id);
+  }
 
-    async findOneById(id: string): Promise<Category> {
-        return categories.find(category => category.id === id)
-    }
+  async create(createCategoryInput: NewCategoryInput): Promise<Category> {
+    const newCategory: Category = {
+      ...createCategoryInput,
+    };
+
+    categories.push(newCategory);
+    console.log(categories);
+    return newCategory;
+  }
 }
