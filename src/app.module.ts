52075ce76bccc81605c5d1a56195d99658a4a8a9
Dennis-Bitoiu <dennis.bitoiu01@gmail.com>
2023-06-29 18:56:41 +0200

    Remove provider and controller from the app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,18 +1,18 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import {GraphQLModule} from '@nestjs/graphql';
-import {ApolloDriver, ApolloDriverConfig} from '@nestjs/apollo';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { join } from 'path';
-import {CategoriesModule} from './categories/categories.module';
+import { CategoriesModule } from './categories/categories.module';
 import { PublicationsModule } from './publications/publications.module';
 
 @Module({
-  imports: [ GraphQLModule.forRoot<ApolloDriverConfig>({
-    driver: ApolloDriver,
-    autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
-  }), CategoriesModule, PublicationsModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+    }),
+    CategoriesModule,
+    PublicationsModule,
+  ],
 })
 export class AppModule {}
