1c707cafd72f5930dee50b9ecfae8a6dbe87128e
Dennis-Bitoiu <dennis.bitoiu01@gmail.com>
2023-06-29 12:28:09 +0200

    Write unit tests for retrieving all the publications

--- src/publications/publications.resolver.spec.ts
+++ src/publications/publications.resolver.spec.ts
@@ -1,19 +1,34 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { PublicationsResolver } from './publications.resolver';
-import { describe, beforeEach, it } from 'node:test';
+import { publications } from '../../data/publications';
+import { PublicationsService } from './publications.service';
+import { CategoriesService } from '../categories/categories.service';
 
 describe('PublicationsResolver', () => {
   let resolver: PublicationsResolver;
+  let service: PublicationsService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [PublicationsResolver],
+      providers: [PublicationsResolver, PublicationsService, CategoriesService],
     }).compile();
 
     resolver = module.get<PublicationsResolver>(PublicationsResolver);
+    service = module.get<PublicationsService>(PublicationsService);
   });
 
-  // it('should be defined', () => {
-  //   expect(resolver).toBeDefined();
-  // });
+  it('should be defined', () => {
+    expect(resolver).toBeDefined();
+  });
+
+  describe('getPublications', () => {
+    it('should return an array of publications', async () => {
+      jest.spyOn(service, 'findAll').mockResolvedValue(publications);
+
+      const result = await resolver.getPublications();
+
+      expect(result).toEqual(publications);
+      expect(service.findAll).toHaveBeenCalled();
+    });
+  });
 });
--- src/publications/publications.service.spec.ts
+++ src/publications/publications.service.spec.ts
@@ -1,6 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { PublicationsService } from './publications.service';
-import { describe, beforeEach } from 'node:test';
+import { publications } from '../../data/publications';
 
 describe('PublicationsService', () => {
   let service: PublicationsService;
@@ -13,7 +13,14 @@ describe('PublicationsService', () => {
     service = module.get<PublicationsService>(PublicationsService);
   });
 
-  // it('should be defined', () => {
-  //   expect(service).toBeDefined();
-  // });
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
+
+  describe('getPublications', () => {
+    it('should return all publications', async () => {
+      const foundPublications = await service.findAll();
+      expect(foundPublications).toEqual(publications);
+    });
+  });
 });
